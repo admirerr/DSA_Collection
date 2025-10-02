d6b2aed52a67cbeda8c2762fe2b264834c062aaa
Silkie Agarwal <silkieagarwal@Silkies-MacBook-Air.local>
2025-10-02 15:19:56 +0530

    added code for longest-substring-without-repeating-characters in cpp

--- CPP/Sliding window/longest-substring-without-repeating-characters.cpp
+++ CPP/Sliding window/longest-substring-without-repeating-characters.cpp
@@ -1,31 +1,30 @@
 // Problem: Longest Substring Without Repeating Characters
 #include <bits/stdc++.h>
 using namespace std;
+class Solution {
+  public:
+    int lengthofLongestSubstring(string s) {
+      vector < int > mpp(256, -1);
 
-int solve(string str) {
+      int left = 0, right = 0;
+      int n = s.size();
+      int len = 0;
+      while (right < n) {
+        if (mpp[s[right]] != -1)
+          left = max(mpp[s[right]] + 1, left);
 
-  if(str.size()==0)
-      return 0;
-  int maxans = INT_MIN;
-  for (int i = 0; i < str.length(); i++) // outer loop for traversing the string
-  {
-    unordered_set < int > set;
-    for (int j = i; j < str.length(); j++) // nested loop for getting different string starting with str[i]
-    {
-      if (set.find(str[j]) != set.end()) // if element if found so mark it as ans and break from the loop
-      {
-        maxans = max(maxans, j - i);
-        break;
+        mpp[s[right]] = right;
+
+        len = max(len, right - left + 1);
+        right++;
       }
-      set.insert(str[j]);
+      return len;
     }
-  }
-  return maxans;
-}
+};
 
 int main() {
   string str = "takeUforward";
-  cout << "The length of the longest substring without repeating characters is " << 
-  solve(str);
+  Solution obj;
+  cout << "The length of the longest substring without repeating characters is " << obj.lengthofLongestSubstring(str);
   return 0;
 }
